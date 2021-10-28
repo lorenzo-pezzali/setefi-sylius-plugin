5da25f8a7225036365b3b1f5414edf0ba3990663
lpezzali <lorenzo.pezzali>
2021-10-28 12:58:11 +0200

    rename plugin

--- src/Controller/GreetingController.php
+++ src/Controller/GreetingController.php
@@ -11,12 +11,12 @@ final class GreetingController extends AbstractController
 {
     public function staticallyGreetAction(?string $name): Response
     {
-        return $this->render('@AcmeSyliusExamplePlugin/static_greeting.html.twig', ['greeting' => $this->getGreeting($name)]);
+        return $this->render('@SetefiSyliusPlugin/static_greeting.html.twig', ['greeting' => $this->getGreeting($name)]);
     }
 
     public function dynamicallyGreetAction(?string $name): Response
     {
-        return $this->render('@AcmeSyliusExamplePlugin/dynamic_greeting.html.twig', ['greeting' => $this->getGreeting($name)]);
+        return $this->render('@SetefiSyliusPlugin/dynamic_greeting.html.twig', ['greeting' => $this->getGreeting($name)]);
     }
 
     private function getGreeting(?string $name): string
